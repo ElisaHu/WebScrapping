b2242cf70b16a0bdaa5084e1d261b622ddde3293
Elisa <yiting_hu@126.com>
2021-05-07 15:51:22 +0800

    get links for each step

--- websiteSearch/dataCollection.py
+++ websiteSearch/dataCollection.py
@@ -1,16 +1,20 @@
+import csv
+
 import pandas as pd
 import urllib.request, urllib.parse
 import re
+import requests
+import lxml
 
 # get numpy array from csv file
-my_csv = pd.read_csv('Revocation.csv')
-column = my_csv['DOC case No.']
+my_csv = pd.read_csv('Overall.csv')
+column = my_csv['DOCNo']
 DOCarray = column[1:].values
 productColumn = my_csv['Product']
 productArray = productColumn[1:].values
 
 
-def AntiDumping(DOC):
+def AntiDumping(DOC, product):
     # get the website link, read content from it
     url = 'https://www.federalregister.gov/documents/search?conditions%5Bterm%5D=' + DOC
     response = urllib.request.urlopen(url)
@@ -34,22 +38,51 @@ def AntiDumping(DOC):
     activation = []
     revocation = []
     for i in webLinkList:
-        if "initiation-of-antidumping-duty" in i:
-            initiation.append(i)
-        if "antidumping-duty-orders" in i:
-            activation.append(i)
-        if "continuation-of-antidumping-duty" in i:
-            revocation.append(i)
-    print("initiation")
-    print(initiation)
-    print("activation")
-    print(activation)
-    print("revocation")
-    print(revocation)
+        productLink = product.replace(' ', '-').lower()
+        if productLink in i:
+            longlink = urllib.request.urlopen(i)
+            longwebContent = str(longlink.read())
+            print(longwebContent)
+            titlePattern = re.compile(
+                r'<div id="metadata_content_area" class="metadata-content-area">\\n.+<h1>(.+?)<\/h1>',
+                flags=re.M)
+            title = titlePattern.findall(longwebContent)[0]
+            if 'Review' in title:
+                continue;
+            if 'Preliminary' in title:
+                continue;
+            if 'Notice' in title:
+                continue;
+            print(title)
+            if 'Initiation of Antidumping Duty' in title:
+                initiation.append(i)
+            if 'Antidumping Duty Orders' in title:
+                activation.append(i)
+                # html = requests.get(url).content
+                # df_list = pd.read_html(html)
+                # df = df_list[-1]
+                # print(df)
+            if 'Revocation of Antidumping' in title:
+                revocation.append(i)
+    if len(initiation) == 0 or len(activation) == 0:
+        print('take a look! we do not have initiation or activation in this product?')
+    if len(activation) > 0:
+        # we should only have 1 activation file per product
+        # find DOC No., Year, Month, Date, HS1, HS2, HS3, HS4, HS5, HS6, HS7, Product, Country, Exporter, ExpAltNm, Producer, PdAltNm,  ProducerID,  AD_CVD, Dumping Margin, Cash Deposit, Action, Source
+        dataFormat = re.compile(r'Publication Date:.+documents\/(.+?)">',
+                flags=re.M)
+        title = dataFormat.findall(longwebContent)[0]
 
-AntiDumping('A-583-080')
+    # print("initiation")
+    # print(initiation)
+    # print("activation")
+    # print(activation)
+    # print("revocation")
+    # print(revocation)
+AntiDumping('A-201-842', 'Large Residential Washers')
 
-# def Countervailing(DOC):
+# todo: change this function to match antidumping duty
+# def Countervailing(DOC, product):
 #     # get the website link, read content from it
 #     url = 'https://www.federalregister.gov/documents/search?conditions%5Bterm%5D=' + DOC
 #     response = urllib.request.urlopen(url)
@@ -72,18 +105,24 @@ AntiDumping('A-583-080')
 #     for i in webLinkList:
 #         if "Initiation" in i and "Initiation" in i:
 
-    # iterating over each DOC Number
-# for DOC in DOCarray:
+
+# iterating over each DOC Number
+# for index in range(0, len(DOCarray)):
+#     # create one csv file for each DOC case
+#     currcsvname = productArray[index]
+#     csvFile = open(currcsvname + '.csv', 'w+')
+#     csvFile.write(["DOC No., Year, Month, Date, HS1, HS2, HS3, HS4, HS5, HS6, HS7, Product, Country, Exporter, ExpAltNm, Producer, PdAltNm,  ProducerID,  AD_CVD, Dumping Margin, Cash Deposit, Action, Source"])
 #     # get the type: AD/CVD -- first char in a string
-#     type = DOC[0]
+#     type = DOCarray[index][0]
 #     if (type == 'A'):
-#         AntiDumping(DOC)
+#         AntiDumping(DOCarray[index], productArray[index], csvFile)
 #     elif (type == 'C'):
-#         Countervailing(DOC)
+#         Countervailing(DOCarray[index], productArray[index], csvFile)
 #     else:
-#         print(DOC)
-    # create one csv file for each DOC case
-    # csvFile = open('overallData.csv', 'w+')
+#         print(DOCarray[index])
+
+    # df = pd.read_csv("example.csv", header=None)
+    # df.to_csv("example.csv", header=["Letter", "Number", "Symbol"], index=False)
 
 # --------unworked code------------------------------------------------------
 # csvFile = open('Revocation.csv', 'w+')
